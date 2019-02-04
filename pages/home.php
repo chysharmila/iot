20401f5fa27f0fd65c1b5e09505852e73364631f
chysharmila <sharmilchudh547@gmail.com>
2019-02-04 15:38:09 +0545

    first page

---
+++ pages/home.php
@@ -0,0 +1,30 @@
+<div class="container">
+	<div class="row">
+		<div class="col-sm-8">
+			<img src="photo/is (1).jpg" width="100%" style="height: 353px;">
+			<span>kichan Room</span>
+			<span class="glyphicon-user"></span>
+
+		</div>
+		<div class="col-sm-4">
+			<img src="photo/is4.jpg" width="100%" style="height: 353px;">
+			<span>Bath-Room</span>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-sm-6">
+			<img src="photo/is (2).jpg" width="100%">
+			<span>Room1</span>
+		</div>
+		<div class="col-sm-6">
+			<img src="photo/is (2).jpg" width="100%">
+			<span> Room2</span>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-sm-12">
+			<img src="photo/is (3).jpg" width="100%" style="height: 353px;">
+			<span>Sitting-Room</span>
+		</div>
+	</div>
+</div>
